59652b49f8145e977bf9094c1005624124430f78
kandersen753 <kurt.t.andersen@gmail.com>
2017-03-29 16:46:54 -0700

    fixed scripts?

--- PA3/build/autoqueue.sh
+++ PA3/build/autoqueue.sh
@@ -1,19 +1,61 @@
 #!/bin/bash
 
-MAX = 1000000
-STEP = 10
+TEST = $(squeue -o"%.18i %.9p %.20j %.8u %.8T %.10M %.91 %.6D %R")
 
-for ((i=100; i<=$MAX; i*=$STEP))
+while [ [ "$TEST" =~ "kandersen-"]]
 do
-	TEST = $(squeue -o"%.18i %.9p %.20j %.8u %.8T %.10M %.91 %.6D %R")
-
-	while [ [ "$TEST" =~ "kandersen-"]]
-	do
-		sleep 1s
-		TEST = $(squeue -o"%.18i %.9p %.20j %.8u %.8T %.10M %.91 %.6D %R")
-	done
-
-	sbatch sequentialBucket.sh $i
-
 	sleep 1s
-done			
+	TEST = $(squeue -o"%.18i %.9p %.20j %.8u %.8T %.10M %.91 %.6D %R")
+done
+
+sbatch sequentialBucket.sh ../bin/inputs/100.txt
+
+sleep 1s
+
+TEST = $(squeue -o"%.18i %.9p %.20j %.8u %.8T %.10M %.91 %.6D %R")
+
+while [ [ "$TEST" =~ "kandersen-"]]
+do
+	sleep 1s
+	TEST = $(squeue -o"%.18i %.9p %.20j %.8u %.8T %.10M %.91 %.6D %R")
+done
+
+sbatch sequentialBucket.sh ../bin/inputs/1000.txt
+
+sleep 1s
+
+TEST = $(squeue -o"%.18i %.9p %.20j %.8u %.8T %.10M %.91 %.6D %R")
+
+while [ [ "$TEST" =~ "kandersen-"]]
+do
+	sleep 1s
+	TEST = $(squeue -o"%.18i %.9p %.20j %.8u %.8T %.10M %.91 %.6D %R")
+done
+
+sbatch sequentialBucket.sh ../bin/inputs/10000.txt
+
+sleep 1s
+
+TEST = $(squeue -o"%.18i %.9p %.20j %.8u %.8T %.10M %.91 %.6D %R")
+
+while [ [ "$TEST" =~ "kandersen-"]]
+do
+	sleep 1s
+	TEST = $(squeue -o"%.18i %.9p %.20j %.8u %.8T %.10M %.91 %.6D %R")
+done
+
+sbatch sequentialBucket.sh ../bin/inputs/100000.txt
+
+sleep 1s
+
+TEST = $(squeue -o"%.18i %.9p %.20j %.8u %.8T %.10M %.91 %.6D %R")
+
+while [ [ "$TEST" =~ "kandersen-"]]
+do
+	sleep 1s
+	TEST = $(squeue -o"%.18i %.9p %.20j %.8u %.8T %.10M %.91 %.6D %R")
+done
+
+sbatch sequentialBucket.sh ../bin/inputs/1000000.txt
+
+sleep 1s			
